a81c106466e56eefb0082affb5bd4aeb590dd73d
Martinus Suherman <martinus.suherman@gmail.com>
2021-02-01 15:35:33 +0700

    Load config dynamically from json file

--- src/main.js
+++ src/main.js
@@ -13,20 +13,25 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './stores'
+import { configureRouter } from './router-async'
+import { configureVuexStore } from './stores/async'
 import './plugins/base'
 import './plugins/chartist'
 import './plugins/vee-validate'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
 
-Vue.config.productionTip = false
+// eslint-disable-next-line no-void
+void (async function () {
+  Vue.config.productionTip = false
+  const router = await configureRouter()
+  const store = await configureVuexStore()
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  i18n,
-  render: h => h(App),
-}).$mount('#app')
+  new Vue({
+    router,
+    store,
+    vuetify,
+    i18n,
+    render: h => h(App),
+  }).$mount('#app')
+})()
---
+++ src/stores/async.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { configureVuexOidc } from './oidc-async'
+
+export async function configureVuexStore () {
+  Vue.use(Vuex)
+
+  const store = new Vuex.Store({
+    state: {
+      barColor: 'rgba(0, 0, 0, .8), rgba(0, 0, 0, .8)',
+      barImage: 'https://demos.creative-tim.com/material-dashboard/assets/img/sidebar-1.jpg',
+      drawer: null,
+    },
+    mutations: {
+      SET_BAR_IMAGE (state, payload) {
+        state.barImage = payload
+      },
+      SET_DRAWER (state, payload) {
+        state.drawer = payload
+      },
+    },
+    actions: {
+
+    },
+    modules: {
+      oidcStore: await configureVuexOidc(),
+    },
+  })
+
+  return store
+}
